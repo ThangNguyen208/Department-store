c195f7263cffea7e1fb1b92d13375b3282bcd6c2
ThangNguyen208 <thangtat208@gmail.com>
2021-02-25 15:09:57 +0700

    share code on github

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,92 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import data from '../data.js';
+import User from '../models/userModel.js';
+import { generateToken, isAuth } from '../utils.js';
+
+/** làm cho code thành module*/
+const userRouter = express.Router();
+
+userRouter.get(
+  '/arse',
+  expressAsyncHandler(async (req, res) => {
+    // await User.remove({});
+    /* insertMany chấp nhận 1 mảng và chèn các đối tượng trên amngr đó trong bộ sưu tập ng dùng  */
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdUsers });
+  })
+);
+
+userRouter.post(
+    '/login',
+    expressAsyncHandler(async (req, res) => {
+      const user = await User.findOne({ email: req.body.email });
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          res.send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user),
+          });
+          return;
+        }
+      }
+      res.status(405).send({ message: 'Invalid email or password' });
+    })
+  );
+
+  userRouter.post(
+    '/register',
+    expressAsyncHandler(async (req, res) => {
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
+      });
+      const createUser = await user.save();
+      res.send({
+        _id: createUser._id,
+        name: createUser.name,
+        email: createUser.email,
+        isAdmin: createUser.isAdmin,
+        token: generateToken(createUser),
+      });
+    })
+  );
+  userRouter.get(
+    '/:id',
+    expressAsyncHandler(async (req, res) => {
+      const user = await User.findById(req.params.id);
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({ message: 'User Not Found' });
+      }
+    })
+  );
+  userRouter.put(
+    '/profile',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+      const user = await User.findById(req.user._id);
+      if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+          user.password = bcrypt.hashSync(req.body.password, 8);
+        }
+        const updatedUser = await user.save();
+        res.send({
+          _id: updatedUser._id,
+          name: updatedUser.name,
+          email: updatedUser.email,
+          isAdmin: updatedUser.isAdmin,
+          token: generateToken(updatedUser),
+        });
+      }
+    })
+  );
+export default userRouter;
